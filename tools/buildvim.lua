4ee057fd8920ff94b67a9f9c5073042db4da5b7d
Xavier Wang <weasley.wx@gmail.com>
2018-11-20 21:05:47 +0800

    update vim build

--- tools/buildvim.lua
+++ tools/buildvim.lua
@@ -1,5 +1,6 @@
 -- options --
-local dstdir = [[\Vim\vim81]]
+local tdir   = [[\Devel]]
+local dstdir = tdir..[[\Vim\vim81]]
 local srcdir = [[\Work\Sources\Vim\src\]]
 local rtdir  = [[\Work\Sources\Vim\runtime\]]
 local flags  = [[DIRECTX=yes IME=yes CSCOPE=yes OLE=yes WINVER=0x0501]]
@@ -7,13 +8,13 @@ local optflags = {
     user     = [[USERNAME=SW USERDOMAIN=SB_DiaoSi_Mo XPM=no NODEFAULTLIB=/nodefaultlib:msvcrt.lib ]],
     sdkdir   = [[SDK_INCLUDE_DIR="C:\Program Files\Microsoft SDKs\Windows\v7.1\Include"]],
     sdkdir64 = [[SDK_INCLUDE_DIR="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include"]],
-    lua      = [[LUA=\Lua53 DYNAMIC_LUA=yes LUA_VER=53]],
-    mz       = [[MZSCHEME=\Racket DYNAMIC_MZSCHEME=yes MZSCHEME_VER=3m_9x82yo]],
-    perl     = [[PERL=\perl DYNAMIC_PERL=yes PERL_VER=522]],
-    python   = [[PYTHON=\python27 DYNAMIC_PYTHON=yes PYTHON_VER=27]],
-    python3  = [[PYTHON3=\python36 DYNAMIC_PYTHON3=yes PYTHON3_VER=36]],
-    ruby     = [[RUBY=\ruby192 DYNAMIC_RUBY=yes RUBY_VER=192 RUBY_VER_LONG=1.9.1]],
-    tcl      = [[TCL=\Tcl DYNAMIC_TCL=yes TCL_VER=86 TCL_VER_LONG=8.6]],
+    lua      = [[LUA=]]..tdir..[[\Lua53 DYNAMIC_LUA=yes LUA_VER=53]],
+    mz       = [[MZSCHEME=]]..tdir..[[\Racket DYNAMIC_MZSCHEME=yes MZSCHEME_VER=3m_9x82yo]],
+    perl     = [[PERL=]]..tdir..[[\perl DYNAMIC_PERL=yes PERL_VER=522]],
+    python   = [[PYTHON=]]..tdir..[[\python27 DYNAMIC_PYTHON=yes PYTHON_VER=27]],
+    python3  = [[PYTHON3=]]..tdir..[[\python37 DYNAMIC_PYTHON3=yes PYTHON3_VER=37]],
+    ruby     = [[RUBY=]]..tdir..[[\ruby192 DYNAMIC_RUBY=yes RUBY_VER=192 RUBY_VER_LONG=1.9.1]],
+    tcl      = [[TCL=]]..tdir..[[\Tcl DYNAMIC_TCL=yes TCL_VER=86 TCL_VER_LONG=8.6]],
     vs_def   = [[DEFINES="/GL /GS- /O2 /Oy /Oi"]],
 }
 local uses = { "vs_def", "sdkdir64", "user", "lua", "perl", "python3", "tcl" }
